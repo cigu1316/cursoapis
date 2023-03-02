e69854c4d783977941c0d6a61a0a68d1b4d3ae3b
gustavo <cigu1966@gamil.com>
2023-03-02 00:25:32 -0300

    1

---
+++ django_base/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path
+from django_base.views import index
+from django.conf import settings
+from django.conf.urls.static import static 
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', index , name='index'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
